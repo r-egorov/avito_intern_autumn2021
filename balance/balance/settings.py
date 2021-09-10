22c78d3c12a59a0cefe803727d18494301f515f2
Ruslan Egorov <r-egorov@outlook.com>
2021-09-10 13:51:39 +0300

    Persist DB with docker-compose

--- balance/balance/settings.py
+++ balance/balance/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "0.0.0.0", "192.168.99.108"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "0.0.0.0"]
 
 
 # Application definition
